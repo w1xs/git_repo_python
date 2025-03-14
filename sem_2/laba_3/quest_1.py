b36e7671c39c238641b1107bd1262812ec2966fb
w1xs <ddmalyshev@yandex.ru>
2025-03-14 22:32:27 +0300

    laba 3 ready

---
+++ sem_2/laba_3/quest_1.py
@@ -0,0 +1,22 @@
+import json
+
+def main():
+    count = 0
+    with open(".\\animals.json", "r") as file:
+        data = json.load(file)
+    print("Информация о птицах: ")
+    for animal in data["animals"]:
+        if animal["animal_type"] == "Bird":
+            for key in animal.keys():
+                if key != "id" and key != "image_link":
+                    print(f"{key}: {animal[key]}")
+            print()
+        if animal["active_time"] == "Diurnal":
+            count += 1
+    min_weight_animal_data = min(data["animals"], key= lambda x: float(x["weight_min"]))
+    min_weight_animal = min_weight_animal_data["name"]
+    print(f"Число дневных животных: {count}")
+    print(f"Животное с наименьшим весом: {min_weight_animal}")
+
+if __name__ == "__main__":
+    main()
---
+++ sem_2/laba_3/quest_2.py
@@ -0,0 +1,46 @@
+import csv
+
+def check_for_correct(number: str):
+    for ch in number:
+        if ch not in ".0123456789":
+            return None
+    return float(number)
+
+def main():
+    print("Введите нижнюю границу дохода:")
+    low_income = input()
+    low_income = check_for_correct(low_income)
+    if low_income is None:
+        print("Введены не верные данные")
+        return
+
+    print("Введите верхнюю границу дохода:")
+    high_income = input()
+    high_income = check_for_correct(high_income)
+    if high_income is None:
+        print("Введены не верные данные")
+        return
+
+
+    with open(".\\countries.csv", "r") as file:
+        data = list(csv.DictReader(file))
+    first_res = []
+    second_res = []
+    for item in data:
+        if float(item["Income"]) >= low_income and float(item["Income"]) <= high_income:
+            first_res.append(item)
+    second_res = sorted(data, key = lambda x: float(x["Inflation"]))
+
+    with open('res_a.csv', mode='w', newline='') as file:
+        fieldnames = ['Country', 'Health Care', 'Income', 'Inflation', 'Life Expectancy']
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        csv_writer.writerows(first_res)
+
+    with open('res_b.csv', mode='w', newline='') as file:
+        fieldnames = ['Country', 'Health Care', 'Income', 'Inflation', 'Life Expectancy']
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        csv_writer.writerows(second_res)
+if __name__ == "__main__":
+    main()
